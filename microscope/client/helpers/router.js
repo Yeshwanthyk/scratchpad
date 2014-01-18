02d7b38e61a3332e904a42ab579fdb24ddc66528
yeshwanthyk <yeshwanth.yk@gmail.com>
2014-01-18 00:20:37 -0500

    Add iron-router and publish-subscribe
    Created layout.html with {{yield}} in it and used pathFor

---
+++ microscope/client/helpers/router.js
@@ -0,0 +1,9 @@
+Router.configure({
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
+    waitOn: function() {return Meteor.subscribe('posts');}
+});
+
+Router.map(function() {
+    this.route('postsList', {path:"/"});
+});
