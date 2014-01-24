f41c5eabe8423fc7affc82c51d28e9bc2314ed45
yeshwanthyk <yeshwanth.yk@gmail.com>
2014-01-23 19:32:12 -0500

    Start working on breadcrumbs.hs

---
+++ learn-you-a-haskell/breadcrumbs.hs
@@ -0,0 +1,9 @@
+{-- The Breadcrumb tutorial you can get to at
+    http://acm3.wustl.edu/functional/hs-breads.php
+
+    Nothing like learning a new language by actually
+    typing and checking the language constructs
+--}
+
+main = do
+    putStrLn "Hello world"
--- learn-you-a-haskell/recursive-fns.hs
+++ learn-you-a-haskell/recursive-fns.hs
@@ -5,3 +5,17 @@ factorial n   = n * factorial(n-1)
 length' :: (Num b) => [a] -> b
 length'[]   = 0
 length'(_:xs) = 1 + length' xs
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+-- More elegant solution
+maximumE :: (Ord a) => [a] -> a
+maximumE [] = error "maximum of empty list"
+maximumE [x] = x
+maximumE (x:xs) = max x (maximumE xs)
