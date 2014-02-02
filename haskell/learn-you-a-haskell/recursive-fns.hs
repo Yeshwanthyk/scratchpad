946ad101ada3af2487d6220db91d714ed247d971
yeshwanthyk <yeshwanth.yk@gmail.com>
2014-02-02 01:04:15 -0500

    Fix syntax errors

--- haskell/learn-you-a-haskell/recursive-fns.hs
+++ haskell/learn-you-a-haskell/recursive-fns.hs
@@ -25,15 +25,22 @@ maximumE (x:xs) = max x (maximumE xs)
 replicate' :: (Num i, Ord i) => i -> a -> [a]
 replicate' n x
     | n <= 0    = []
-    | otherwise x:replicate' (n-1) x
+    | otherwise = x:replicate' (n-1) x
+
+-- Take
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0    = []
+take' _ []      = []
+take' n (x:xs) = x : take' (n-1) xs
 
 -- Reverse
 reverse' :: [a] -> [a]
 reverse' [] = []
-reverse' x:xs = reverse' xs ++ [x]
+reverse' (x:xs) = reverse' xs ++ [x]
 
 --zip
 zip' :: [a] -> [b] -> [(a,b)]
 zip' _ [] = []
 zip' [] _ = []
-zip' (x:xs) (y:ys) = (x,y) : zip' (xs,ys)
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
