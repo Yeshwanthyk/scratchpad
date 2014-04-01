a15aa1c595eb08756c712a9fe0e99add92f35e37
yeshwanthyk <yeshwanth.yk@gmail.com>
2014-04-01 00:06:06 -0400

    Lean differences between tail recursion and normal recursion

--- scala/progfun/.worksheet/src/week1.session.scala
+++ scala/progfun/.worksheet/src/week1.session.scala
@@ -16,8 +16,21 @@ object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport.
         
 	    sqrtIter(1.0)
     
-    };System.out.println("""sqrt: (x: Double)Double""");$skip(13); val res$0 = 
+    };System.out.println("""sqrt: (x: Double)Double""");$skip(159); 
+    
+    // Calls itself.. So tail recursive call so uses
+    // constant stack frame
+    def gcd(a: Int, b: Int): Int =
+    	if (b == 0) a else gcd(b, a % b);System.out.println("""gcd: (a: Int, b: Int)Int""");$skip(97); 
 
-    sqrt(4);System.out.println("""res0: Double = """ + $show(res$0))}
+		// Not tail-recursive
+		def factorial(n: Int): Int =
+			if (n == 0) 1 else n * factorial(n-1);System.out.println("""factorial: (n: Int)Int""");$skip(17); val res$0 = 
+				
+    sqrt(4);System.out.println("""res0: Double = """ + $show(res$0));$skip(20); val res$1 = 
+    
+    gcd(14,21);System.out.println("""res1: Int = """ + $show(res$1));$skip(22); val res$2 = 
+    
+    factorial(5);System.out.println("""res2: Int = """ + $show(res$2))}
     
 }
--- scala/progfun/src/week1/session.sc
+++ scala/progfun/src/week1/session.sc
@@ -17,7 +17,21 @@ object session {
 	    sqrtIter(1.0)
     
     }                                             //> sqrt: (x: Double)Double
+    
+    // Calls itself.. So tail recursive call so uses
+    // constant stack frame
+    def gcd(a: Int, b: Int): Int =
+    	if (b == 0) a else gcd(b, a % b)          //> gcd: (a: Int, b: Int)Int
 
+		// Not tail-recursive
+		def factorial(n: Int): Int =
+			if (n == 0) 1 else n * factorial(n-1)
+                                                  //> factorial: (n: Int)Int
+				
     sqrt(4)                                       //> res0: Double = 2.0000000929222947
+    
+    gcd(14,21)                                    //> res1: Int = 7
+    
+    factorial(5)                                  //> res2: Int = 120
     
 }
