999baa8b7d2c3f40b8f27f5aa15085390f984c06
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2019-10-29 02:26:51 -0400

    Solve LCA

---
+++ algorithms/leetcode/235-lowest-common-ancestor.py
@@ -0,0 +1,8 @@
+def lca(root, p, q):
+    while root:
+        if p.val > root.val and q.val > root.val:
+            root = root.right
+        elif p.val < root.val and q.val < root.val:
+            root = root.left
+        else:
+            return root
