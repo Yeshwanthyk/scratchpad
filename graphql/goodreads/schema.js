ee8ae5e06a6c6193d720c4e001f058c7f3e35770
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-04-25 17:03:27 -0400

    Add lazy fetching

--- graphql/goodreads/schema.js
+++ graphql/goodreads/schema.js
@@ -17,11 +17,11 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     title: {
       type: GraphQLString,
-      resolve: (xml) => xml.title[0],
+      resolve: (xml) => xml.GoodreadsResponse.book[0].title[0],
     },
     isbn: {
       type: GraphQLString,
-      resolve: (xml) => xml.isbn[0],
+      resolve: (xml) => xml.GoodreadsResponse.book[0].isbn[0],
     },
   }),
 });
@@ -36,7 +36,21 @@ const AuthorType = new GraphQLObjectType({
     },
     books: {
       type: new GraphQLList(BookType),
-      resolve: (xml) => xml.GoodreadsResponse.author[0].books[0].book,
+      resolve: (xml) => {
+        const ids = xml.GoodreadsResponse.author[0].books[0].book.map(
+          (elem) => elem.id[0]._
+        );
+
+        return Promise.all(
+          ids.map((id) =>
+            fetch(
+              `https://www.goodreads.com/book/show/${id}.xml?key=Kr23aNZUus1hAvktOi4dww`
+            )
+              .then((response) => response.text())
+              .then(parseXML)
+          )
+        );
+      },
     },
   }),
 });
