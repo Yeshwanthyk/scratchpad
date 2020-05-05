90ce94bccd61acae8f9dd9350af7854bed8722a6
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2020-05-05 17:43:15 -0400

    Learn variables Mental model just js

---
+++ js/justjs/04.js
@@ -0,0 +1,26 @@
+//Counting
+
+//undefined
+
+//only one undefined in js. represents unintentionally mistakes. if a variable is unthethered, its also undefined
+
+//null
+//only one null. can talk about intentional errors. most people avoid both
+//
+
+//Boolean
+
+//only one true or false and the wires connect to only these
+
+//Numbers
+
+//In JS universe, numbers are picked to the closest percision (floating point math)
+//Special numbers - Nan, Infinity, -0, - Infinity
+//
+let scale = 0;
+let a = 1 / scale; // Infinity
+let b = 0 / scale; // NaN
+let c = -a; // -Infinity
+let d = 1 / c; // -0
+
+
