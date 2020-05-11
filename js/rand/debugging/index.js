0bdd5b763383f603255122f898b3c342155dbf41
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2020-05-10 23:50:51 -0400

    Test debugging using VScode
    https://charlesagile.com/debug-series-nodejs-browser-javascript

---
+++ js/rand/debugging/index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const url = require("url");
+
+const hostname = "127.0.0.1";
+const port = 3456;
+const serverUrl = `http://${hostname}:${port}`;
+
+const server = http.createServer((req, res) => {
+  const { name } = url.parse(req.url, true).query;
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end(`Hello, ${name}!\n`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at ${serverUrl}`);
+});
