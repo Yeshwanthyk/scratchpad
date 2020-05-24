0552575c76c36438b838ceb5c2221329e51fd41f
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2020-05-23 23:48:50 -0400

    Display posts - keystone

--- keystone/pages/index.js
+++ keystone/pages/index.js
@@ -1,8 +1,57 @@
 import { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+	static async getInitialProps() {
+		let response = await axios.get('http://localhost:3000/api/posts');
+		return { posts: response.data };
+	}
+
 	render() {
-		return <div>Hello</div>;
+		return (
+			<div className="container">
+				<style jsx>{`
+					.header {
+						padding: 16px 16px;
+					}
+					.content {
+						padding: 16px 16px;
+					}
+					.post {
+						margin-bottom: 16px;
+					}
+				`}</style>
+				<div className="header">
+					<h1>Keystone Next Example</h1>
+				</div>
+				<div className="content">
+					{this.props.posts.map((post, i) => {
+						let button;
+						if (post.image) {
+							button = (
+								<img className="img-fluid" src={post.image.secure_url} />
+							);
+						} else {
+							button = <img className="img-fluid" src={post.image} />;
+						}
+
+						return (
+							<div className="post" key={i}>
+								<div className="row">
+									<div className="col-12 col-md-4">{button}</div>
+									<div className="col-12 col-md-8">
+										<h2>{post.title}</h2>
+										<div
+											dangerouslySetInnerHTML={{ __html: post.content.brief }}
+										></div>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		);
 	}
 }
 
