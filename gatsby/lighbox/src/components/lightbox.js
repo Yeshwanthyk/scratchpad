350fa0a7974db8d2880e48ec97ae89d784f2e674
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-07-13 00:09:19 -0400

    Add lightbox

--- gatsby/lighbox/src/components/lightbox.js
+++ gatsby/lighbox/src/components/lightbox.js
@@ -1,23 +1,67 @@
-import React, { Component } from "react"
+import React, { Component, Fragment } from "react"
 import PropTypes from "prop-types"
 import Img from "gatsby-image"
+import styled from "styled-components"
+import { Dialog } from "@reach/dialog"
+import "@reach/dialog/styles.css"
+
+const LightboxContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 5px;
+`
+
+const PreviewButton = styled.button`
+  background: transparent;
+  border: none;
+  padding: 0;
+  margin: 0;
+`
 
 export default class Lightbox extends Component {
   static propTypes = {
-    carImages: PropTypes.array.isRequired,
+    brieflyImages: PropTypes.array.isRequired, // eslint-disable-line
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showLightbox: false,
+      selectedImage: null,
+    }
   }
 
   render() {
     const { brieflyImages } = this.props
+    const { selectedImage, showLightbox } = this.state
     return (
-      <div>
-        {brieflyImages.map(image => (
-          <Img
-            key={image.node.childImageSharp.fluid.src}
-            fluid={image.node.childImageSharp.fluid}
-          />
-        ))}
-      </div>
+      <Fragment>
+        <LightboxContainer>
+          {brieflyImages.map(image => (
+            <PreviewButton
+              key={image.node.childImageSharp.fluid.src}
+              type="button"
+              onClick={() =>
+                this.setState({ showLightbox: true, selectedImage: image })
+              }
+            >
+              <Img fluid={image.node.childImageSharp.fluid} />
+            </PreviewButton>
+          ))}
+        </LightboxContainer>
+        {showLightbox && (
+          <Dialog>
+            <Img fluid={selectedImage.node.childImageSharp.fluid} />
+            <button
+              type="button"
+              onClick={() => this.setState({ showLightbox: false })}
+            >
+              Close
+            </button>
+          </Dialog>
+        )}
+      </Fragment>
     )
   }
 }
