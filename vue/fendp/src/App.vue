56921938f190b63db589ae34b6ca0deeb9009b5d
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-07-20 01:05:13 -0400

    Try vue project using vue-cli
    https://codepen.io/sdras/pen/owaWwd

---
+++ vue/fendp/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+    <h1>Your Upcoming Destinations</h1>
+    <div class="location-contain">
+      <div class="locations" v-for="location in locations">
+        <location :location="location">
+          <h2>{{ location.name }}</h2>
+        </location>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Location from "./components/Location.vue";
+
+export default {
+  name: "app",
+  components: {
+    Location
+  },
+  data() {
+    return {
+      locations: [
+        {
+          name: "moscow",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg",
+          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`
+        },
+        {
+          name: "paris",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg",
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+        },
+        {
+          name: "rome",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg",
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`
+        },
+        {
+          name: "paris",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg",
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  width: 100vw;
+  height: 100vh;
+  font-family: "NTR", sans-serif;
+  background: #eee;
+}
+
+h1 {
+  text-align: center;
+}
+
+.location-contain {
+  display: flex;
+  justify-content: center;
+}
+
+.place {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 320px;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+  h2 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    margin: 10px;
+    align-self: center;
+  }
+}
+</style>
---
+++ vue/fendp/src/components/Location.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="place">
+    <img :src="location.img" width="235" height="300" />
+    <slot></slot>
+    <p>{{ location.desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["location"]
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
