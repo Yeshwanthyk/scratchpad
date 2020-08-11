1ab0d8d82d70a63ebb3d3b327d233108a1ec4a48
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-08-10 23:47:47 -0400

    Add routes

--- vue/real-world-vue/.eslintrc.js
+++ vue/real-world-vue/.eslintrc.js
@@ -4,14 +4,15 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    "plugin:vue/essential",
+    "plugin:prettier/recommended", // we added this line
+    "@vue/prettier"
   ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  parserOptions: {
+    parser: "babel-eslint"
   }
-}
+};
