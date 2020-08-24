354610c4b7205043db5a02c829735456a56aec60
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-08-23 20:07:56 -0400

    Add favicon and home page

--- eleventy-filter-coffee-starter/.eleventy.js
+++ eleventy-filter-coffee-starter/.eleventy.js
@@ -1,6 +1,6 @@
 module.exports = (config) => {
-  // Set directories to pass through to the dist folder
-  config.addPassthroughCopy('./src/images');
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
 
   return {
     markdownTemplateEngine: 'njk',
--- eleventy-filter-coffee-starter/src/index.md
+++ eleventy-filter-coffee-starter/src/index.md
@@ -1,1 +1,7 @@
-Hello world
+---
+layout: 'layouts/home.html'
+---
+
+A blank 11ty project, with a dose of strong filter coffee.
+
+![an image of filter coffee](./images/filter-coffee.jpg)
