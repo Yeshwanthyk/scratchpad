30c85cea2cc897c222e0b1e367f0ef5afa28758e
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-08-23 23:20:04 -0400

    Extra possibilities for canvas-sketch

--- generative/genart/sketch.js
+++ generative/genart/sketch.js
@@ -2,6 +2,9 @@ const canvasSketch = require("canvas-sketch");
 
 const settings = {
 	dimensions: [2048, 2048]
+	// orientation: "landscape",
+	// units: "in",
+	// pixelsPerInch: 300
 };
 
 const sketch = () => {
@@ -10,11 +13,11 @@ const sketch = () => {
 		context.fillRect(0, 0, width, height);
 
 		context.beginPath();
-		context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false);
+		context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
 		context.fillStyle = "red";
 		context.fill();
 
-		context.lineWidth = 20;
+		context.lineWidth = width * 0.05;
 		context.strokeStyle = "green";
 		context.stroke();
 	};
