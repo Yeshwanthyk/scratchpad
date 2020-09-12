0638ae63183b5f8ab84f5a73e4218c0eb3581c65
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-09-11 22:45:36 -0400

    eslint for react
    npm install -D babel-eslint eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react

--- js/react/pure-react/src/App.js
+++ js/react/pure-react/src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet.js";
 
 // like blueprint/stamp
 const App = () => {
@@ -15,4 +11,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
