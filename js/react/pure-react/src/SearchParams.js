f6d449db8bd6d5ce688df8c9ad720fe1aa24b258
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-09-14 09:29:20 -0400

    Add API PETS

--- js/react/pure-react/src/SearchParams.js
+++ js/react/pure-react/src/SearchParams.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("dog");
 
   return (
     <div className="search-params">
@@ -14,6 +17,45 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            name="animal"
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => {
+              return (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            name="breed"
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={!breeds.length}
+          >
+            <option>All</option>
+            {breeds.map((breedStr) => {
+              return (
+                <option key={breedStr} value={breedStr}>
+                  {breedStr}
+                </option>
+              );
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
