703356e565cbcc9a5bbb9edd02123b608e8fc56c
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-09-24 16:00:12 -0400

    Add css

--- gatsby/master-gatsby/gatsby/src/components/Layout.js
+++ gatsby/master-gatsby/gatsby/src/components/Layout.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
 import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 const Layout = ({ children }) => (
   <div>
+    <GlobalStyles />
+    <Typography />
     <Nav />
     {children}
     <Footer />
